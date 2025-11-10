3e9e176674e81bea8732a069391e3f8e5d35b65a
Lucas Sauvage <lucassauvage@Lucass-MacBook-Pro-10.local>
2025-11-10 12:47:14 -0600

    intitial front end files

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,24 @@
-const App = () => {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CustomerInterface } from './pages/CustomerInterface';
+import { CashierInterface } from './pages/CashierInterface';
+import { ManagerInterface } from './pages/ManagerInterface';
+import { AdminButton } from './components/AdminButton';
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Customer/Self service interface is the default */}
+        <Route path="/" element={<CustomerInterface />} />
+        <Route path="/customer" element={<CustomerInterface />} />
+        {/* Admin views no protection needed for development -> Make sure to add passcode next spring - LS*/}
+        <Route path="/cashier" element={<CashierInterface />} />
+        <Route path="/manager" element={<ManagerInterface />} />
+      </Routes>
+      {/* Admin button always visible in bottom left */}
+      <AdminButton />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
