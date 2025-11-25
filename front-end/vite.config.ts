61dfc200919b292020225857af91ee0c6e062225
Leenser Biju <leenser@tamu.edu>
2025-11-24 21:10:35 -0600

    changed to render

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://bobateashopsite.onrender.com/',
         changeOrigin: true,
       },
     },
